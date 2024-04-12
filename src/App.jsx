1bb71a89021523af82bbe560f74d4a71e1ef62d1
Blanca Tirado <blanquitatiradocamargo@gmail.com>
2024-04-12 15:33:20 -0400

    primera

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+//codigo de la 1ra parte
+/*import './App.css';
+import { Button } from './components/Button';
+
+function App() {
+  const buttonText = "Count : ";
+
+  return (
+    <>
+      <h1>HOLA DESDE VITE</h1>
+      <Button text={buttonText} />
+    </>
+  );
+}
+
+export default App;*/
+
+//codigo de la 2da parte
+
+import './App.css';
+import { List } from './components/List';
+import { WapperList } from './components/WapperList';
+
+function App() {
+  return (
+    <>
+      <WapperList>
+      <List/>  
+      </WapperList>
+    </>
+  )
+}
+
+export default App;
